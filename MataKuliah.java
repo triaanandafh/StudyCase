eadeac45d987135331ee69d7eca3c20cb442264b
triaanandafh <fadiltria51@gmail.com>
2025-04-21 14:49:03 +0700

    class mata kuliah

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,3 +1,18 @@
 public class MataKuliah {
-    
+
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    MataKuliah(String kdMK, String nmMK, int sks) {
+        this.kodeMK = kdMK;
+        this.namaMK = nmMK;
+        this.sks = sks;
+    }
+
+    void tampilMatakuliah() {
+        System.out.println("Kode Mata Kuliah: " + kodeMK);
+        System.out.println("Nama Mata Kuliah: " + namaMK);
+        System.out.println("SKS: " + sks);
+    }
 }
